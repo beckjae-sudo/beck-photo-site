5259623a172ceb1afbbf8d3ae7e23f20e4e0d464
Jae Beck <beck.jae@gmail.com>
2026-08-28 00:51:45 -0400

    Add DeleteObjectsCommand to S3 imports

--- src/app/admin/actions.ts
+++ src/app/admin/actions.ts
@@ -1,6 +1,12 @@
 "use server";
 
-import { S3Client, GetObjectCommand, PutObjectCommand, DeleteObjectCommand } from "@aws-sdk/client-s3";
+import {
+  S3Client,
+  PutObjectCommand,
+  GetObjectCommand,
+  DeleteObjectCommand,
+  DeleteObjectsCommand,
+} from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import { cookies } from "next/headers";
 
