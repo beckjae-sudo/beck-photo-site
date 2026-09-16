fca700d7ca1cb4ea22095a68911fe15442a8f8ea
Jae Beck <beck.jae@gmail.com>
2026-09-16 12:06:29 -0400

    Add allowedDevOrigins to next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  allowedDevOrigins: [
+    "192.168.86.45",
+    "192.168.86.45:3000",
+    "localhost:3000",
+  ],
 };
 
 export default nextConfig;
